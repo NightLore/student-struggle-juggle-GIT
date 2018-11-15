26f6968f03716904e9b02c6169d8cce76ddab557
Andrew Sulistio <andrew@raftesalo.net>
2018-11-14 20:14:48 -0800

    Removed Calculator and moved main screen to logic

---
+++ src/logic/Theme_Manager/ThemeManager.java
@@ -0,0 +1,66 @@
+package logic.Theme_Manager;
+
+import javafx.scene.paint.Color;
+import logic.Theme_Manager.Theme;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+
+/**
+ * ThemeManager implements the package for Theme Manager, this enables other parts of the program to quickly and easily
+ * access different themes
+ *
+ * @author Andrew Sulistio on 11/14/18.
+ * @version student-struggle-juggle-GIT
+ */
+public class ThemeManager {
+
+    private String defaultTheme = "Default";
+
+    private Theme currentTheme = null;
+    private HashMap<Integer, Theme> themes = new HashMap<>();
+
+    public ThemeManager(){
+        try {
+            currentTheme = new Theme(defaultTheme);
+            themes.put(defaultTheme.hashCode(),currentTheme);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addTheme(String name){
+        try {
+            themes.put(name.hashCode(),new Theme(name));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            //TODO alert user that theme was not found
+        }
+    }
+
+    public Theme setTheme(String name){
+        Theme temp = themes.get(name.hashCode());
+        if (temp == null) {
+            throw new NoSuchElementException("Theme: " + name + " not found! Perhaps it wasn't added yet?");
+        }
+        else{
+            currentTheme = temp;
+            return temp;
+        }
+    }
+
+    public Theme removetheme(String name){
+        return themes.remove(name.hashCode());
+    }
+
+    /**
+     * obtains currently active theme/last loaded theme
+     * @return null if no theme loaded
+     */
+    public Theme getActiveTheme(){
+        return currentTheme;
+    }
+}
