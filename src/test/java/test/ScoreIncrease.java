e2bb4b6b7908b4c8faf392daa3415d749557d335
unknown <hluengas@calpoly.edu>
2018-12-04 21:43:56 -0800

    added test: testScoreIncrementOnSuccessfulJuggle(). also added two getter methods to game.java

---
+++ src/test/java/test/ScoreIncrease.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import logic.Game;
+import logic.JuggleObject;
+
+/**
+ * @author Henry Luengas
+ */
+public class ScoreIncrease {
+
+    /**
+     * @author Henry Luengas
+     */
+    @Test
+    public void testScoreIncrementOnSuccessfulJuggle()
+    {
+        Game game = new Game(null);
+        
+        List<JuggleObject> objects = Arrays.asList(new JuggleObject[] {
+                new JuggleObject(game.getPaddle().getX(), Game.FRAME_HEIGHT, 25, 1.0, 0, 0, null)
+        });
+        game.checkCollisions(objects);
+        
+        int score = game.getInfo().getScore();
+        
+        assertEquals(1, score);
+    }
+}
